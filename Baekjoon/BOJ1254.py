b3fab2bac5b02798509510cbd862cbd1f1b7ece4
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2022-06-15 11:45:14 +0900

    Update BOJ1254.py

--- Baekjoon/BOJ1254.py
+++ Baekjoon/BOJ1254.py
@@ -8,6 +8,6 @@ result = ''
 
 for i in range(len(S)):
     result = S[i:] # S를 i부터 끝까지 복사
-    if len(result) == 1 or result == result[::-1]: # result의 길이가 1인 case : abab, result == result[::-1]은 result가 펠린드롬
+    if len(result) == 1 or result == result[::-1]: # result의 길이가 1인 경우는 뒤에 한 문자만 추가, result == result[::-1]은 result가 펠린드롬
         print(len(S)+i) # S의 길이 + 추가한 문자의 길이
         break
