df6cc32ea8b7be7668ab87130726d002aaee99ec
baejunil <bjo6300@naver.com>
2022-07-19 16:52:53 +0900

    Update BOJ8958.py

--- Baekjoon/BOJ8958.py
+++ Baekjoon/BOJ8958.py
@@ -6,16 +6,18 @@ lst = []
 for _ in range(n):
     lst.append(input())
 
-cnt = 0
-score = 0
+cnt = 0 # 연속된 O의 개수
+score = 0 # 총 점수
 
 for i in range(n):
-    for j in lst[i]:
+    for j in lst[i]: # lst에 넣은걸 차례대로 계산
         if j == "O":
             cnt += 1
             score += cnt
         else:
             cnt = 0
+
     print(score)
-    score=0
-    cnt = 0
+
+    score=0 # 다음을 위해 초기화
+    cnt = 0 # 다음을 위해 초기화
