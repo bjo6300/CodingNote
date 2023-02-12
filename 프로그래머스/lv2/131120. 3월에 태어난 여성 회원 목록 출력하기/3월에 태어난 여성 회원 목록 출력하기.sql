2bee4f553f086084cf62283a9731e780af5a168f
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2023-02-12 15:23:52 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/lv2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,8 +1,11 @@
 select 
-MEMBER_ID, 
-MEMBER_NAME, 
-GENDER, 
-DATE_FORMAT(DATE_OF_BIRTH, '%Y-03-%d') as DATE_OF_BIRTH
+    MEMBER_ID, 
+    MEMBER_NAME, 
+    GENDER, 
+    DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH
 from MEMBER_PROFILE
-where DATE_OF_BIRTH LIKE '%-03-%' and gender = 'W' and tlno!= ''
+where 
+    DATE_OF_BIRTH LIKE '%-03-%' 
+    and gender = 'W' 
+    and tlno != ''
 order by MEMBER_ID;
