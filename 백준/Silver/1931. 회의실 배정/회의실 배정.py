6859065cf5684ce3bab7dd3b083de0a1904320be
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2023-03-29 16:51:26 +0900

    [Silver I] Title: 회의실 배정, Time: 268 ms, Memory: 59204 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,17 +1,15 @@
-N = int(input())
-time = []
+import sys
+input = sys.stdin.readline
 
-for _ in range(N):
-  start, end = map(int, input().split())
-  time.append([start, end])
+n = int(input())
+meeting_list = [list(map(int, input().split())) for _ in range(n)]
 
-time = sorted(time, key=lambda a: a[0]) # 시작 시간을 기준으로 오름차순
-time = sorted(time, key=lambda a: a[1]) # 끝나는 시간을 기준으로 다시 오름차순
+meeting_list.sort(key = lambda x : (x[1], x[0]))
 
 last = 0 # 회의의 마지막 시간을 저장할 변수
 conut = 0 # 회의 개수를 저장할 변수
 
-for i, j in time:
+for i, j in meeting_list:
   if i >= last: # 시작시간이 회의의 마지막 시간보다 크거나 같을경우
     conut += 1
     last = j
