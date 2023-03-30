a5d985459da8ea6add5ad46c4670d0d01b422b47
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2023-03-30 19:28:31 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,12 @@
+def solution(num):
+    cnt = 0
+    while True:
+        if num == 1: 
+            return cnt
+        if cnt >= 500: return -1
+        
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num = num * 3 + 1
+        cnt += 1
