42d47a5abd77d51e43d587231f861412c756595b
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2023-04-04 13:07:30 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 388 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,31 @@
+while True:
+    x=input().rstrip()
+    if x=='.':
+        break
+    stack=[]
+    result=True
+    for i in x:
+        if i=='[' or i=='(':
+            stack.append(i)
+        elif i==']':
+            if not stack:
+                result=False
+                break
+            if stack[-1]=='[':
+                stack.pop()
+            else:
+                result==False
+                break
+        elif i==')':
+            if not stack:
+                result=False
+                break
+            if stack[-1]=='(':
+                stack.pop()
+            else:
+                result==False
+                break
+    if result and not stack:
+        print('yes')
+    else:
+        print('no')
