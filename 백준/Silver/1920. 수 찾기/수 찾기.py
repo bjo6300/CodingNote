ca463f32525f70980dd53045016124afc44042f2
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2023-04-28 20:44:06 +0900

    [Silver IV] Title: 수 찾기, Time: 204 ms, Memory: 50092 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,15 @@
+# 수 찾기
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+N = set(map(int, input().split()))
+m = int(input())
+M = list(map(int, input().split()))
+
+for i in M:
+    if i in N:
+        print(1)
+    else:
+        print(0)
