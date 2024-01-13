dfc7fa3d637beb1414f75a4183949e59651ba0b4
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2024-01-14 00:30:11 +0900

    [level Lv.1] Title: 대충 만든 자판, Time: 30.89 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/Lv.1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -6,11 +6,11 @@ def solution(keymap, targets):
         
         for char in word:   # 한 단어의 개별 문자에 대해
             flag = False    # 목표 문자열을 작성할수 있는지 없는지 판단하기 위함
-            time = 101      # keymap의 원소의 길이가 최대 100이기 때문에 101로 설정
+            time = 999      # keymap의 원소의 길이가 최대 100이기 때문에 101로 설정
             # keymap에 있는 모든 원소를 반복하면서 가장 적게 누를수 있는 char(문자)를 찾음
             for key in keymap:      
                 if char in key:	# key(문자열)안에 char(문자)가 존재하면
-                    time = min(key.index(char)+1, time)
+                    time = min(key.find(char)+1, time)
                     flag = True    # 목표 문자열을 작성할 수 있음
                     
             if not flag:           # 만약 keymap을 전부다 돌았는데도 문자를 찾지 못하면
