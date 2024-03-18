3546a5f9186bc6e29ef06597707ade626fc4c27f
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2024-03-19 01:26:00 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -2,27 +2,25 @@ def solution(want, number, discount):
     answer = 0
     
     for i in range(len(discount)):
-        if discount[i] not in want: # 원하는 제품이 없는 경우
+        if discount[i] not in want: # 처음에 원하는 제품이 없는 경우
             continue
         
-        flag = 1
-        temp_number = number.copy()  # deepcopy 대신 얕은 복사 사용
+        flag = 1 # 모두 할인 받는 경우 : 1
+        temp_number = number.copy()  
         
-        numbers_len = sum(number)
-        
-        for j in range(i, numbers_len + i): # want 길이만큼 반복
+        for j in range(i, 10 + i):
             if j >= len(discount):  # 인덱스 오류 방지
                 flag = 0
                 break
             
-            if discount[j] in want:
+            if discount[j] in want: # 범위 내에 원하는 제품이 있는 경우
                 idx = want.index(discount[j])
                 temp_number[idx] -= 1
                 
                 if temp_number[idx] < 0:
                     flag = 0
                     break
-            else:
+            else: # 범위 내에 원하는 제품이 없는 경우
                 flag = 0
                 break
         
